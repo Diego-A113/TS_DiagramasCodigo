77297f931e4379308be19e7b78975fcd82b40ba4
AnaiMenendez <anaimene@espol.edu.ec>
2026-06-10 13:54:58 -0500

    Clase indicente con sus atributos

---
+++ ts/src/main/java/com/tsdiagramas/Indicente.java
@@ -0,0 +1,17 @@
+package com.tsdiagramas;
+
+import java.sql.Date;
+
+public class Indicente {
+
+    protected int id;
+    protected Date fecha_reportado;
+    protected String estado;
+    protected String titulo;
+    protected String descripcion;
+    
+    protected Date fecha_cerrado;
+    protected String tipo;
+
+
+}
